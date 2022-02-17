24092b21d4cab2a874f7b5d58a56386b0c51829c
Urius <patura4@gmail.com>
2022-02-17 15:37:28 +0500

    add lesson-1-5.cpp

---
+++ lesson-1-5.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main() {
+    printf(" * * * *\n");
+    printf("* * * * \n");
+    printf(" * * * *\n");
+    printf("* * * * \n");
+    printf(" * * * *\n");
+    printf("* * * * \n");
+    printf(" * * * *\n");
+    printf("* * * * \n");
+
+    printf("==========\n");
+
+    printf(" * * * *\n"
+           "* * * *\n"
+           " * * * *\n"
+           "* * * *\n"
+           " * * * *\n"
+           "* * * *\n"
+           " * * * *\n"
+           "* * * *");
+
+    return 0;
+}
+
+
