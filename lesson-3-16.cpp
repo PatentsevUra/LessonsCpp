68310750a8baff276366c58348c3d53b1479adf2
Urius <patura4@gmail.com>
2022-02-23 14:30:05 +0500

    add lesson-3-16.cpp

---
+++ lesson-3-16.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+int main() {
+    int x1 = 1;
+    int y1 = 3;
+    int x2 = 2;
+    int y2 = 3;
+
+    if (x1 == x2 || y1 == y2) {
+        std::cout << "Attack!!!" << std::endl;
+    }
+
+    return 0;
+}
+
